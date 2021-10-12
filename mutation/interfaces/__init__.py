9334e6c35a6ee0aea6322963bdb1ad7e5dd79e22
Eder Lima <devilsouls666@gmail.com>
2021-10-12 19:42:45 -0300

    fix: add ABC to base state machine class

--- mutation/interfaces/__init__.py
+++ mutation/interfaces/__init__.py
@@ -1,6 +1,7 @@
 from typing import Any, Dict, List
+from abc import ABC
 
 
-class AbstractStateMachine:
+class AbstractStateMachine(ABC):
     INITIAL_STATE: Any = None
     MUTATION_SCHEMA: List[Dict[str, Any]] = None
